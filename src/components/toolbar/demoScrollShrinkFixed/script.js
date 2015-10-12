68f14e9e9cb863e0e7e417c0c969934566570e12
Walter Mazza <wjmazza@gmail.com>
2015-10-12 09:57:22 -0700

    mdToolbar: added  attribute; allows a minimum height for toolbar when shrunk

---
+++ src/components/toolbar/demoScrollShrinkFixed/script.js
@@ -0,0 +1,7 @@
+var app = angular.module('toolbarDemo3', ['ngMaterial']);
+
+app.controller('FixedShrinkApp', function() {
+  var vm = this;
+  vm.title = 'Application Name';
+  vm.subtitle = 'Page Subtitle';
+});
